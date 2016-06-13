e599acfda8de6ae591703cd0df6ea50651380745
Alex Weidmann <alex@twinsoftech.com>
2016-06-12 19:22:03 -0600

    add httpoison

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule ReverseProxy.Mixfile do
   def application do
     [mod: {ReverseProxy, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :httpoison]]
   end
 
   # Specifies which paths to compile per environment.
@@ -36,7 +36,8 @@ defmodule ReverseProxy.Mixfile do
      {:phoenix_html, "~> 2.4"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:httpoison, "~> 0.8"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
--- web/router.ex
+++ web/router.ex
@@ -3,10 +3,6 @@ defmodule ReverseProxy.Router do
 
   pipeline :browser do
     plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
   end
 
   pipeline :api do
@@ -19,7 +15,8 @@ defmodule ReverseProxy.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
+  # When you add your current production API route-by-route
+  # This is where you will setup the Phoenix API to handle it
   # scope "/api", ReverseProxy do
   #   pipe_through :api
   # end
